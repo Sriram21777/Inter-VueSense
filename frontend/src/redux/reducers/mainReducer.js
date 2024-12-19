1f31b0f8cab5a95202b2deb8e21164d4f637c7bb
Nani <m.jnanayashwanth@Yashs-MacBook-Air.local>
2024-12-19 14:39:23 +0530

    Intitial Commit

---
+++ frontend/src/redux/reducers/mainReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+    loggedIn: false,
+    isInterviewer: false,
+    username: '',
+    name: '',
+}
+
+const mainReducer = (state = initialState, actions) => {
+    switch (actions.type) {
+        case 'UPDATE_USER_DETAILS': {
+            return { ...state, ...actions.payload }
+        }
+        default: return state
+    }
+}
+
+export default mainReducer
